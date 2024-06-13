27fc9e315fbf629d035203cea8992d0d813fd128
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-06-13 13:00:10 +0500

    started aboutus

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import LogoCards from "./components/LogoCards";
+import AboutUs from "./components/AboutUs";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <LogoCards />
+      <AboutUs />
     </>
   );
 };
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <section>
+      <div className="flex flex-col items-center mt-40">
+        <p className="max-w-lg mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl gradient-class">
+          WHAT WE OFFER
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
