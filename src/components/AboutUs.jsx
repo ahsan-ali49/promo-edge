10c78b048bc6de89bdc27edb8be93b31b5325457
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-06-15 13:49:16 +0500

    added aboutus

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,7 @@ import AboutLogo2 from "../assets/images/AboutLogo2.png";
 const AboutUs = () => {
   return (
     <section>
-      <div className="flex flex-col items-center mt-40">
+      <div className="flex flex-col items-center mt-40 mb-40">
         <p className="max-w-lg mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl gradient-class">
           WHAT WE OFFER
         </p>
@@ -16,22 +16,98 @@ const AboutUs = () => {
           Our logo services help you get the best leaving the competition in the
           backyard and zip through the market of logo design companies
         </p>
-        <div className="flex flex-row w-full justify-around mt-10">
-          <div className="relative">
-            <div className="w-64 h-52 border border-sky-500 rounded-lg">
-              <img src={AboutLogo1} alt="clean logo" className="mx-auto my-8" />
+        <div className="flex flex-row w-full mt-10">
+          <div className="relative top-2/4 w-3/6 flex flex-col items-center justify-center">
+            <div className="absolute top-48 left-48 w-64 h-52 border border-sky-500 rounded-lg mb-8 flex items-center">
+              <div className="about-log-div-2 w-full h-full rounded-lg flex items-center justify-center">
+                <img
+                  src={AboutLogo1}
+                  alt="clean logo"
+                  className="opacity-100"
+                />
+              </div>
             </div>
-            <div className="absolute top-48 left-48 w-64 h-52 border border-sky-500 rounded-lg">
+            <div className="absolute top-96 left-96 w-64 h-52 border border-sky-500 rounded-lg">
               <div className="about-log-div-2 w-full h-full rounded-lg flex items-center justify-center">
                 <img
                   src={AboutLogo2}
                   alt="clean logo"
-                  className="mx-auto my-8 opacity-100"
+                  className="opacity-100"
                 />
               </div>
             </div>
           </div>
-          <div className="text-white">World</div>
+
+          <div className="flex flex-col text-white w-3/6">
+            <div className="flex">
+              <p className="max-w-lg mt-6 font-light text-gray-500 lg:mb-0 md:text-lg lg:text-3xl font-bold gradient-class">
+                01.
+              </p>
+              <div className="flex flex-col ml-8">
+                <h1 className="mb-4 text-2xl tracking-tight leading-none md:text-5xl xl:text-3xl dark:text-white">
+                  Emblems
+                </h1>
+                <p className="max-w-2xl text-left mb-6 font-light text-gray-500 lg:mb-8 md:text-md lg:text-md dark:text-gray-300 px-0 mx-0">
+                  Emblems, also known as badge logos, are typically reminiscent
+                  of crests. They combine text and symbolic imagery to form
+                  ornate designs with a traditional feel.
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <p className="max-w-lg mt-6 font-light text-gray-500 lg:mb-0 md:text-lg lg:text-3xl font-bold gradient-class">
+                02.
+              </p>
+              <div className="flex flex-col ml-8">
+                <h3 className="mb-4 text-2xl tracking-tight leading-none md:text-5xl xl:text-3xl dark:text-gray-400">
+                  Combination Marks
+                </h3>
+                <p className="max-w-2xl text-left mb-6 font-light text-gray-500 lg:mb-8 md:text-md lg:text-md dark:text-gray-400 px-0 mx-0">
+                  This type of logo combines (surprise, surprise) images with
+                  words. For example, a combination logo could consist of an
+                  icon with a wordmark, or a mascot with a letterform and so on.
+                  Some brands have one main logo in the form of a combination
+                  mark, while occasionally splitting up the text and imagery to
+                  better suit various contexts.
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <p className="max-w-lg mt-6 font-light text-gray-500 lg:mb-0 md:text-lg lg:text-3xl font-bold gradient-class">
+                03.
+              </p>
+              <div className="flex flex-col ml-8">
+                <h3 className="mb-4 text-2xl tracking-tight leading-none md:text-5xl xl:text-3xl dark:text-gray-400">
+                  Wordmarks
+                </h3>
+                <p className="max-w-2xl text-left mb-6 font-light text-gray-500 lg:mb-8 md:text-md lg:text-md dark:text-gray-400 px-0 mx-0">
+                  Wordmarks (a.k.a. logotypes) consist of the company’s name,
+                  written in a certain typeface. While this may sound very
+                  straightforward, as Steve Jobs said, “Simple can be harder
+                  than complex.” To successfully learn how to design a logo, you
+                  need to pay extra attention to every little detail.
+                </p>
+              </div>
+            </div>
+            <div className="flex">
+              <p className="max-w-lg mt-6 font-light text-gray-500 lg:mb-0 md:text-lg lg:text-3xl font-bold gradient-class">
+                04.
+              </p>
+              <div className="flex flex-col ml-8">
+                <h3 className="mb-4 text-2xl tracking-tight leading-none md:text-5xl xl:text-3xl dark:text-gray-400">
+                  Monogram logos
+                </h3>
+                <p className="max-w-2xl text-left mb-6 font-light text-gray-500 lg:mb-8 md:text-md lg:text-md dark:text-gray-400 px-0 mx-0">
+                  Lettermarks (a.k.a. monogram logos) are typography logos that
+                  are made up of the brand’s initials. In most cases, brands
+                  that have monogram logos are referred to by their abbreviated
+                  version when speaking, like IBM and NASA (when was the last
+                  time you heard someone say National Aeronautics and Space
+                  Administration?).
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
