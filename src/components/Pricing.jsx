caaea134c906ce196c08c0d47b4f9c5e18f19383
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-06-16 14:05:05 +0500

    added pricing component

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -62,40 +62,99 @@ const Pricing = () => {
         "Free Email Signature",
         "48 to 72 hours TAT",
         "All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)",
-        "100% Unique Design Guarantee",
-        "100% Satisfaction Guarantee",
-        "100% Money Back Guarantee *",
+        // "100% Unique Design Guarantee",
+        // "100% Satisfaction Guarantee",
+        // "100% Money Back Guarantee *",
       ],
       postNote: "Add on; $50 for expedited services",
     },
     {
       title: "Business Logo",
       price: "448",
-      features: [],
+      features: [
+        "UNLIMITED Logo Design Concepts",
+        "By 8 Award Winning Designers",
+        "UNLIMITED Revisions",
+        "2 Stationary Design Sets (Business Card, Letterhead, Envelope)",
+        "Icon Design",
+        "Double Sided Flyer Design / Bi-Fold Brochure",
+        "FREE MS Word Letterhead",
+        "Free Email Signature",
+        "All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)",
+        // "100% Unique Design Guarantee",
+        // "100% Satisfaction Guarantee",
+        // "100% Money Back Guarantee *",
+      ],
       postNote: "Add on; $50 for expedited services",
     },
     {
       title: "Gold Logo",
       price: "848",
-      features: [],
+      features: [
+        "UNLIMITED Logo Design Concepts",
+        "By 8 Award Winning Designers",
+        "Icon Design",
+        "UNLIMITED Revisions",
+        "2 Stationary Design Sets (Business Card, Letterhead, Envelope)",
+        "FREE MS Word Letterhead",
+        "Free Email Signature",
+        "3 Page Custom Website",
+        "2 Stock Photos",
+        "2 Banner Designs",
+        "jQuery Slider",
+        "All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)",
+        // "100% Unique Design Guarantee",
+        // "100% Satisfaction Guarantee",
+        // "100% Money Back Guarantee *",
+      ],
       postNote: "Add on; $50 for expedited services",
     },
     {
       title: "Elite Logo",
       price: "499",
-      features: [],
+      features: [
+        "1 Animated Logo Design Concept",
+        "1 Dedicated Logo Designer",
+        "3 Revisions",
+        "Turn Around 24-48 hrs",
+        "100% Ownership Rights",
+        "100% Satisfaction Guarantee",
+        "100% Unique Design Guarantee",
+        "100% Money Back Guarantee *",
+      ],
       postNote: "Add on; $50 for expedited services",
     },
     {
       title: "Business Logo",
       price: "999",
-      features: [],
+      features: [
+        "3 Animated Logo Design Concepts",
+        "3 Dedicated Logo Designer",
+        "Unlimited Revisions",
+        "Turn Around 24-48 hrs",
+        "100% Ownership Rights",
+        "100% Satisfaction Guarantee",
+        "100% Unique Design Guarantee",
+        "100% Money Back Guarantee *",
+      ],
       postNote: "Add on; $50 for expedited services",
     },
     {
       title: "Gold Logo",
       price: "1999",
-      features: [],
+      features: [
+        "3 Unique 3D Logo Concepts",
+        "Light Effects and VFX",
+        "Fully Rendered",
+        "Multiple 3D Angles",
+        "By 3 Award Winning Designers",
+        "72 hours Turnaround Time",
+        "UNLIMITED Revisions",
+        "100% Ownership Rights",
+        "100% Unique Design Guarantee",
+        "100% Satisfaction Guarantee",
+        "100% Money Back Guarantee *",
+      ],
       postNote: "Add on; $50 for expedited services",
     },
   ];
@@ -109,18 +168,14 @@ const Pricing = () => {
         <h1 className="mb-4 text-4xl text-center tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
           That Work For You!
         </h1>
-        <div className="grid lg:grid-rows-3 md-grid-rows-4 sm:grid-rows-3 max-w-screen-xl px-4 py-8 mx-auto lg:py-0 lg:grid-cols-12 w-full text-white">
-          <div className="lg:col-span-4">
-            <PricingCard />
-          </div>
-          <div className="lg:col-span-4">2</div>
-          <div className="lg:col-span-4">3</div>
-          <div className="lg:col-span-4">4</div>
-          <div className="lg:col-span-4">5</div>
-          <div className="lg:col-span-4">6</div>
-          <div className="lg:col-span-4">7</div>
-          <div className="lg:col-span-4">8</div>
-          <div className="lg:col-span-4">9</div>
+        <div className="grid gap-y-5 lg:grid-rows-3 md-grid-rows-4 sm:grid-rows-3 max-w-screen-xl px-4 py-8 mx-auto lg:py-0 lg:grid-cols-12 w-full text-white">
+          {pricingData.map((priceData, index) => {
+            return (
+              <div className="lg:col-span-4" key={index}>
+                <PricingCard priceData={priceData} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,124 +1,39 @@
 import React from "react";
+import { FiArrowUpRight } from "react-icons/fi";
 
-const PricingCard = () => {
+const PricingCard = ({ priceData }) => {
   return (
-    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-        Standard plan
+    <div className="w-full h-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 lg:pb-0 dark:bg-gray-800 dark:border-gray-700 flex flex-col">
+      <h5 className="mb-4 text-xl font-medium text-white dark:text-white frame-19 w-full text-center w-full h-12">
+        {priceData.title}
       </h5>
-      <div class="flex items-baseline text-gray-900 dark:text-white">
-        <span class="text-3xl font-semibold">$</span>
-        <span class="text-5xl font-extrabold tracking-tight">49</span>
-        <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-          /month
+      <div className="flex items-baseline text-gray-900 dark:text-white mb-4">
+        <span className="text-3xl font-semibold">$</span>
+        <span className="text-5xl font-extrabold tracking-tight">
+          {priceData.price}
         </span>
       </div>
-      <ul role="list" class="space-y-5 my-7">
-        <li class="flex items-center">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
+      <ul role="list" className="list-disc space-y-2 my-7 flex-grow">
+        {priceData.features.map((feature, index) => (
+          <li
+            key={index}
+            className="text-base font-normal leading-tight text-gray-500 dark:text-gray-300 ms-3 custom-list-marker"
           >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-            2 team members
-          </span>
-        </li>
-        <li class="flex">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-            20GB Cloud storage
-          </span>
-        </li>
-        <li class="flex">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-            Integration help
-          </span>
-        </li>
-        <li class="flex line-through decoration-gray-500">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 ms-3">
-            Sketch Files
-          </span>
-        </li>
-        <li class="flex line-through decoration-gray-500">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 ms-3">
-            API Access
-          </span>
-        </li>
-        <li class="flex line-through decoration-gray-500">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 ms-3">
-            Complete documentation
-          </span>
-        </li>
-        <li class="flex line-through decoration-gray-500">
-          <svg
-            class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span class="text-base font-normal leading-tight text-gray-500 ms-3">
-            24×7 phone & email support
-          </span>
-        </li>
+            {feature}
+          </li>
+        ))}
       </ul>
       <button
         type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+        className="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        id="get-started"
       >
-        Choose plan
+        START PROJECT
+        <FiArrowUpRight className="inline-block ml-2" />
       </button>
+      <p className="my-4 text-base font-normal text-gray-300">
+        {priceData.postNote}
+      </p>
     </div>
   );
 };
